a96a4f2270c33acd694ced8924d4f8f28d75bbf3
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-03-22 01:03:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+var kittens = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendKitten(newKitten){
+  kittens.push(newKitten);
+}
+
+function destructivelyPrependKitten(newKitten){
+  kittens.unshift(newKitten)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(newKitten){
+  kittens = [newKitten, ...kittens];
+}
+
+function prependKitten(newKitten){
+  kittens = 
+}
